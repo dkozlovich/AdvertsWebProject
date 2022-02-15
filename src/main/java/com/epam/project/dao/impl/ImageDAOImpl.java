a1e9d32ee8e9b618233eafd76f25c0b6fc7ee45a
Dmitry <d>
2022-02-15 19:01:19 +0300

    fix

--- src/main/java/com/epam/project/dao/impl/ImageDAOImpl.java
+++ src/main/java/com/epam/project/dao/impl/ImageDAOImpl.java
@@ -30,24 +30,24 @@ public class ImageDAOImpl implements ImageDAO {
 
     private static final String ADD_IMAGE = "INSERT INTO project.images (advertImage, advertID) values (?,?)";
 
+    private static final String GET_ADVERT_ID_BY_IMAGE_ID = "SELECT (advertID) FROM project.images WHERE id=?";
+
     private static final String GET_BY_ADVERT_ID = "SELECT * FROM project.images WHERE advertID=?";
 
     private static final String DELETE_IMAGE = "DELETE FROM project.images WHERE id=?";
+
+    private static final String UPDATE_ADVERT_DATE = "UPDATE project.adverts SET modified=? WHERE id=?";
 
     @Override
     public Image add(BufferedImage advertImage, int advertId) throws DAOException {
         Image image = new Image();
         int id = 0;
-
-
         try (Connection con = ConnectionPool.getInstance().getConnection();
              PreparedStatement stmt = con.prepareStatement(ADD_IMAGE, Statement.RETURN_GENERATED_KEYS)) {
-
             ByteArrayOutputStream baos = new ByteArrayOutputStream();
             ImageIO.write(advertImage, "jpg", baos);
             baos.flush();
             byte[] imageInByte = baos.toByteArray();
-
             stmt.setBytes(1, imageInByte);
             stmt.setInt(2, advertId);
             int affectedRows = stmt.executeUpdate();
@@ -56,21 +56,24 @@ public class ImageDAOImpl implements ImageDAO {
                 {
                     if (rs.next()) {
                         id = rs.getInt(1);
+                        image.setId(id).setAdvertImage(advertImage).setAdvertId(advertId);
                     }
                 }
             }
+            updateAdvertDate(advertId);
             return image;
         } catch (Exception e) {
             throw new DAOException(e);
         }
     }
-
     @Override
     public void delete(int id) throws DAOException {
         try (Connection con = ConnectionPool.getInstance().getConnection();
              PreparedStatement stmt = con.prepareStatement(DELETE_IMAGE)) {
+            Integer advertId = getAdvertIdByImageId(id);
             stmt.setInt(1, id);
             stmt.executeUpdate();
+            updateAdvertDate(advertId);
         } catch (Exception e) {
             throw new DAOException(e);
         }
@@ -85,7 +88,6 @@ public class ImageDAOImpl implements ImageDAO {
             ResultSet rs = stmt.executeQuery();
             while (rs.next()) {
                 Image image = new Image();
-
                 byte[] imageInByte = rs.getBytes(3);
                 ByteArrayInputStream bais = new ByteArrayInputStream(imageInByte);
                 BufferedImage advertImage = ImageIO.read(bais);
@@ -94,6 +96,33 @@ public class ImageDAOImpl implements ImageDAO {
                 images.add(image);
             }
             return images;
+        } catch (Exception e) {
+            throw new DAOException(e);
+        }
+    }
+
+    private void updateAdvertDate(int advertId) throws DAOException {
+        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+        try (Connection con = ConnectionPool.getInstance().getConnection();
+             PreparedStatement stmt = con.prepareStatement(UPDATE_ADVERT_DATE);) {
+            stmt.setTimestamp(1, timestamp);
+            stmt.setInt(2, advertId);
+            stmt.executeUpdate();
+        } catch (Exception e) {
+            throw new DAOException(e);
+        }
+    }
+
+    private int getAdvertIdByImageId(int id) throws DAOException {
+        Integer advertId = null;
+        try (Connection con = ConnectionPool.getInstance().getConnection();
+             PreparedStatement stmt = con.prepareStatement(GET_ADVERT_ID_BY_IMAGE_ID)) {
+            stmt.setInt(1, id);
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()) {
+                advertId = rs.getInt(1);
+            }
+            return advertId;
         } catch (Exception e) {
             throw new DAOException(e);
         }
